445f6d4e43e231eada7fd165ab4581ace15cc137
snehalbawankule <snehalbawankule2000@gmail.com>
2023-01-03 16:54:52 +0530

    commit

--- src/components/login/login.styled.ts
+++ src/components/login/login.styled.ts
@@ -1,10 +1,10 @@
 import styled from "styled-components";
-
+const pxToRem = (size: number) => `${size / 16}rem`;
 const Forget = styled.button`
   font-family: "Poppins";
-  font-size: 20px;
-  margin-left: 194px;
-  margin-top: 15px;
+  font-size: ${pxToRem(20)};
+  margin-left: ${pxToRem(194)};
+  margin-top: ${pxToRem(15)};
   text-decoration-line: underline;
   border: none;
   background: none;
